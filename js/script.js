830838ee9f29b994a473232c9e2e8fa6b363badf
zammera <zammerallen@gmail.com>
2025-03-25 00:53:23 -0400

    Lots of work on the make list

--- js/script.js
+++ js/script.js
@@ -1,44 +1,66 @@
-$(function () {
-    var myModal = document.getElementById('myModal')
-    var myInput = document.getElementById('myInput')
+const listNames = [
+    {"name":"Taco Tuesday"},
+    {"name":"Pizza Party"}
+];
+if (!localStorage.getItem("listNames")) {
+    localStorage.setItem("listNames", JSON.stringify(listNames));
+}
 
-    myModal.addEventListener('shown.bs.modal', function () {
-    myInput.focus()
-    })
-    
-    $( "#btn1" ).click(function () {
-        let popup = `<div class="modal" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalToggleLabel">Modal 1</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Show a second modal and hide this one with the button below.
-      </div>
-      <div class="modal-footer">
-        <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Open second modal</button>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">Modal 2</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Hide this modal and show the first with the button below.
-      </div>
-      <div class="modal-footer">
-        <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Back to first</button>
-      </div>
-    </div>
-  </div>
-</div>`;
-    $("#Popup").append(popup);
-    });
+
+//aim to change this but wanted to get this functional and pretty
+
+function olMakeList() {
+
+}
+
+function createNewList() {
+    let listName = document.getElementById('name').value;
+    let storedLists = JSON.parse(localStorage.getItem("listNames")) || [];
+
+    if (listName == "" ) {
+        console.log("empty list cannot be created")
+    } else {
+        createListCard(listName);
+        storedLists.push({ name: listName });
+        localStorage.setItem("listNames", JSON.stringify(storedLists));
+        document.getElementById('name').value = ""; 
+    }
+};
+
+function createListCard(listName) {
+    var addNewList = `<div class="card oldList" style="width: 15rem;" id="${ listName }">
+        <div class="card-body">
+            <h5 class="card-title"> ${ listName } </h5>
+            <input class="btn btn-custom-color list-btn" type="submit" value="Edit">
+            <input class="btn btn-danger list-btn" type="reset" value="Delete" onclick="deleteList('${ listName }')">
+        </div>
+    </div>`;
+    Container = document.getElementById('listContainer');  
+    console.log("List: " + listName + " added to the list container")
+    Container.innerHTML += addNewList; 
+}
+
+function deleteList(name) {
+    let element = document.getElementById(name);
+    let storedLists = JSON.parse(localStorage.getItem("listNames")) || [];
+    if (element) {
+        element.remove();
+        storedLists = storedLists.filter(list => list.name !== name);
+        localStorage.setItem("listNames", JSON.stringify(storedLists));
+    } else {
+        console.error("Element with ID " + name + " not found.");
+    }
+}
+
+$(function() {
+    if ($('body').is('#makeList')) {
+        const storedLists = JSON.parse(localStorage.getItem("listNames")) || [];
+
+        storedLists.forEach(list => {
+            console.log(list.name);
+            createListCard(list.name);
+        });
+    }
+
+   
 });
